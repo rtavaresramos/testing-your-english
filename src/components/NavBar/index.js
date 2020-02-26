d80a89518793e14403f5bf8dda2886d89e82a1fd
rtavaresramos <rtavareskxd99@gmail.com>
2020-02-26 20:42:58 -0300

    updating the navbrand to be able as a button

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Div , NavBrand } from './styles';
 import { Row } from '../styleGrid/base';
+import {Link} from 'react-router-dom'
 
 export default class NavBar extends Component {
   render() {
     return (
             <Div>
                 <Row>
-                    <NavBrand>Teste Dev Frontend</NavBrand>
+                    <Link to="/"><NavBrand>Teste Dev Frontend</NavBrand></Link>
                 </Row>
             </Div>
     )
